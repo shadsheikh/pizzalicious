0592416a9f84fdd5d01399bf71d3355139d905ba
Shad Sheikh <shadsheikh3107@gmail.com>
2024-05-19 00:25:48 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: 'Roboto Mono, monospace'
+    },
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }]
+      },
+      colors: {
+        pizza: "#584825"
+      },
+      backgroundImage: {
+        'pizza': "url('/public/pizza.jpeg')",
+      }
+    },
+  },
+
+  plugins: [],
+}
